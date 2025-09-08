cae094e759f9bb489f0d18532d1539343b19a647
Antoine D. <antoine@utopios.net>
2025-09-08 16:43:14 +0200

    Ajout de la demonstration des slots et des watchers

---
+++ project-demo-slots/src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { reactive, ref, watch } from 'vue';
+import Card from './components/Card.vue';
+
+  const monTexte = ref<string>('');
+  const newPeople = reactive({
+    firstname: '',
+    lastname: ''
+  });
+
+  watch(monTexte, (txt: string) => {
+    console.log(txt);
+    
+  });
+
+  watch(() => newPeople.firstname, (newFirstname) => {
+    console.log(newFirstname);
+    
+  });
+
+  const clickHandler = () => {
+    console.log("J'ai été cliqué!");
+  }
+</script>
+
+<template>
+  <input type="text" v-model="monTexte">
+  <hr>
+  <div>
+    <label for="firstname">Firstname: </label>
+    <input type="text" id="firstname" v-model="newPeople.firstname">
+  </div>
+  <div>
+    <label for="lastname">Lastname: </label>
+    <input type="text" id="lastname" v-model="newPeople.lastname">
+  </div>
+  <hr>
+  <Card>
+    <template v-slot:header>
+      <h2>{{ newPeople.firstname }}</h2>
+    </template>
+    <template v-slot:default>
+      <img src="https://i.redd.it/the-damascus-goat-theyre-beautiful-as-youngsters-then-as-v0-9914rtwgz5oc1.jpg?width=600&format=pjpg&auto=webp&s=4957e251ef83cd223369a83b62130d0f14f7e467" alt="Damascus Goat">
+    </template>
+    <template v-slot:footer>
+      <button @click="clickHandler">Click me!</button>
+    </template>
+  </Card>
+</template>
+
+<style scoped></style>
---
+++ project-demo-slots/src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card-container">
+        <div class="card-header">
+            <slot name="header">
+                <h3>Card Header</h3>
+            </slot>
+        </div>
+        <div class="card-body">
+            <slot name="default">
+                <div class="grey-box"></div>
+            </slot>
+        </div>
+        <div class="card-footer">
+            <slot name="footer">
+                <span>Card footer</span>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .grey-box {
+        background-color: grey;
+        height: 250px;
+        aspect-ratio: 1 / 1;
+    }
+
+</style>
