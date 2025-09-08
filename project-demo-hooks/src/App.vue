8216e37cc745fcdd78894f4908ab5f3f37681fc7
Antoine D. <antoine@utopios.net>
2025-09-08 16:59:21 +0200

    Démonstration de l'utilisation des hooks et des références de template

---
+++ project-demo-hooks/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+
+  const isVisible = ref(false);
+</script>
+
+<template>
+  <button @click="() => isVisible = !isVisible">Toggle visibility</button>
+  <hr>
+  <!-- <ChildComponent v-show="isVisible"/> -->
+  <ChildComponent v-if="isVisible"/>
+</template>
+
+<style scoped></style>
---
+++ project-demo-hooks/src/components/ChildComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import { onMounted, onBeforeUnmount, ref, onUpdated, useTemplateRef } from 'vue';
+
+    const maVariable = ref<string>('');
+    const monInputRef = useTemplateRef('inputBasic')
+
+  onMounted(() => {
+    console.log('Le composant ChildComponent est apparu !');
+    monInputRef.value?.focus();
+  })
+
+  onBeforeUnmount(() => {
+    console.log('Le composant ChildComponent s\'en va...');
+    
+})
+
+onUpdated(() => {
+    console.log('Le composant ChildComponent s\'update!');
+    
+  })
+</script>
+
+<template>
+    <div class="component">
+        <div>ChildComponent!</div>
+        <input type="text" v-model="maVariable" ref="inputBasic">
+    </div>
+</template>
+
+<style scoped>
+</style>
